cd8417025b47ccca5a22ac23ba77a79bf8dd9ad2
Alex8943 <65241585+Alex8943@users.noreply.github.com>
2021-04-05 02:52:11 +0200

    add a restController

--- src/main/java/com/example/demo/Controller/Restcontroller.java
+++ src/main/java/com/example/demo/Controller/Restcontroller.java
@@ -6,12 +6,53 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 public class Restcontroller {
+    @Autowired
+    ContactService contactService;
+
+    @GetMapping("/contacts")
+    public List<Contact> contact() {
+        return contactService.readAll();
+    }
+
+    @PostMapping("/contacts")
+    public ResponseEntity<Contact> createContacts(@RequestBody Contact contact) {
+        Contact newKontakt = new Contact(contact.getKontaktID(), contact.getFornavn(), contact.getEfternavn(), contact.getAdresse(), contact.getHusNr(), contact.getTlfnumb());
+        contactService.create(newKontakt);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("location", "/products/" + contact.getKontaktID());
+        return new ResponseEntity<>(newKontakt, headers, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/contacts/{id}")
+    public Contact getContactByID(@PathVariable("id") int id) {
+        return contactService.getKontakt(id);
+    }
+
+    @DeleteMapping("/contacts/{id}")
+    public void deleteContactByID(@PathVariable("id") int id) {
+        contactService.delete(id);
+    }
+
+    @PutMapping("/contact/{id}")
+    public ResponseEntity<Contact> contactUpdate(@PathVariable("id") int id, @RequestBody Contact kontaktDetails) {
+        Contact contact = contactService.getKontakt(id);
+
+        contact.setKontaktID(kontaktDetails.getKontaktID());
+        contact.setFornavn(kontaktDetails.getFornavn());
+        contact.setEfternavn(kontaktDetails.getEfternavn());
+        contact.setAdresse(kontaktDetails.getAdresse());
+        contact.setHusNr(kontaktDetails.getHusNr());
+        contact.setTlfnumb(kontaktDetails.getTlfnumb());
+
+        final Contact updatedKontakt = contactService.update(contact);
+
+        return ResponseEntity.ok(updatedKontakt);
+    }
 
 }
